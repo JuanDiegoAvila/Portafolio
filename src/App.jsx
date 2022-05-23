eb7950ae38b54a2d3ea89185d50a2e511bd61ee0
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-05-23 16:06:32 -0600

    Informacion personal y laboral

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './styles/App.css'
 import Intro from './Intro'
 import Lenguajes from './Lenguajes'
 import Software from './Software'
+import SobreMi from './SobreMi'
 
 const App = () => (
   <div className="general-container">
     <Intro />
+    <SobreMi />
     <Lenguajes />
     <Software />
   </div>
---
+++ src/SobreMi.jsx
@@ -0,0 +1,27 @@
+import './styles/SobreMi.css'
+
+const SobreMi = () => (
+  <div className="sobre-mi-container">
+    <h1>Sobre Mí</h1>
+    <div className="sobre-mi-texto">
+      <h2>Experiencia</h2>
+      <p>
+        Estudio en la Universidad del Valle de Guatemala y estoy cursando mi 3er año
+        en Ingeniería en Ciencias de la Computación y Tecnologías de la Información.
+        Tengo experiencia en desarrollo y diseño de videojuegos, desarrollo web tanto
+        de frontend como de backend y manejo
+        de bases de datos. Trabajo como freelancer en la empresa RealizeService en la cual
+        apoyo con el desarrollo de la página web de Miniso en Guatemala.
+      </p>
+      <h2>Intereses personales</h2>
+      <p>
+        Me gusta mucho la música y tocar piano en mis tiempos libres. Asimismo, laboralmente
+        aspiro a estudiar en el exterior y tener tanta experiencia en distintas áreas de la
+        tecnología como sea posible. Me llama mucho la atención el desarrollo web, especialmente
+        el area de frontend.
+      </p>
+    </div>
+  </div>
+)
+
+export default SobreMi
