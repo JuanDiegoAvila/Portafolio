8ac1ad192a368e60d0e127e29c7608255537e06d
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-05-23 17:03:46 -0600

    Container de informacion personal

--- src/SobreMi.jsx
+++ src/SobreMi.jsx
@@ -1,9 +1,48 @@
 import './styles/SobreMi.css'
+import Botones from '../public/img/buttons.png'
 
 const SobreMi = () => (
   <div className="sobre-mi-container">
     <h1>Sobre Mí</h1>
     <div className="sobre-mi-texto">
+      <div className="barra-sobre-mi">
+        <div className="botones-salida" style={{ backgroundImage: `url(${Botones})` }} />
+      </div>
+      <p>
+        PS C:\Users\JuanDiegoAvila\SobreMi
+        {'>'}
+        <span style={{ color: 'yellow' }}> npm </span>
+        run dev
+        <p style={{ color: '#5691ba' }}>
+          vite v2.9.9
+          <span style={{ color: '#14950d' }}>
+            {' '}
+            dev server running at:
+          </span>
+        </p>
+        <p>
+          {'> '}
+          Local:
+          {' '}
+          <span style={{ color: '#5691ba' }}>
+            http://localhost:
+            <span style={{ color: '#57bfbf' }}>3000</span>
+            /
+          </span>
+
+        </p>
+        <p>
+          {'> '}
+          Network:
+          {' '}
+          <span style={{ color: '#6c6c6c' }}>
+            use `--host` to expose
+          </span>
+        </p>
+
+        <p>ready in 2014ms.</p>
+      </p>
+      <p> ----------------------------------------------------------------- </p>
       <h2>Experiencia</h2>
       <p>
         Estudio en la Universidad del Valle de Guatemala y estoy cursando mi 3er año
